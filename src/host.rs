8ab96f02c339d6319074c1c26b0d7e1b8e42c970
Jiaxiao Zhou <jiazho@microsoft.com>
2022-06-28 01:47:31 -0700

    added error handling
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- src/host.rs
+++ src/host.rs
@@ -2,12 +2,53 @@ pub mod exec {
     use wasmtime::Trap;
     #[allow(unused_imports)]
     use wit_bindgen_wasmtime::{anyhow, wasmtime};
+    #[derive(Clone)]
+    pub struct Observable<'a> {
+        pub rd: &'a str,
+        pub key: &'a str,
+    }
+    impl<'a> std::fmt::Debug for Observable<'a> {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            f.debug_struct("Observable")
+                .field("rd", &self.rd)
+                .field("key", &self.key)
+                .finish()
+        }
+    }
+    #[derive(Clone)]
+    pub enum Error {
+        ErrorWithDescription(String),
+    }
+    impl std::fmt::Debug for Error {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            match self {
+                Error::ErrorWithDescription(e) => f
+                    .debug_tuple("Error::ErrorWithDescription")
+                    .field(e)
+                    .finish(),
+            }
+        }
+    }
     pub trait Exec: Sized {
         type Context: Ctx;
-        fn events_get(caller: wasmtime::Caller<'_, Self::Context>) -> Result<i32, Trap>;
+        fn events_get(caller: wasmtime::Caller<'_, Self::Context>, _arg0: i32) -> Result<(), Trap>;
 
-        fn events_exec(caller: wasmtime::Caller<'_, Self::Context>, _arg0: i32)
-            -> Result<(), Trap>;
+        fn events_listen(
+            caller: wasmtime::Caller<'_, Self::Context>,
+            arg0: i32,
+            arg1: i32,
+            arg2: i32,
+            arg3: i32,
+            arg4: i32,
+            arg5: i32,
+        ) -> Result<(), Trap>;
+
+        fn events_exec(
+            caller: wasmtime::Caller<'_, Self::Context>,
+            arg0: i32,
+            arg1: i64,
+            arg2: i32,
+        ) -> Result<(), Trap>;
 
         fn drop_events(
             caller: wasmtime::Caller<'_, Self::Context>,
@@ -27,13 +68,28 @@ pub mod exec {
         linker.func_wrap(
             "exec",
             "events::get",
-            move |caller: wasmtime::Caller<'_, T::Context>| T::events_get(caller),
+            move |mut caller: wasmtime::Caller<'_, T::Context>, arg0: i32| {
+                T::events_get(caller, arg0)
+            },
+        )?;
+        linker.func_wrap(
+            "exec",
+            "events::listen",
+            move |mut caller: wasmtime::Caller<'_, T::Context>,
+                  arg0: i32,
+                  arg1: i32,
+                  arg2: i32,
+                  arg3: i32,
+                  arg4: i32,
+                  arg5: i32| {
+                T::events_listen(caller, arg0, arg1, arg2, arg3, arg4, arg5)
+            },
         )?;
         linker.func_wrap(
             "exec",
             "events::exec",
-            move |caller: wasmtime::Caller<'_, T::Context>, _arg0: i32| {
-                T::events_exec(caller, _arg0)
+            move |mut caller: wasmtime::Caller<'_, T::Context>, arg0: i32, arg1: i64, arg2: i32| {
+                T::events_exec(caller, arg0, arg1, arg2)
             },
         )?;
         linker.func_wrap(
