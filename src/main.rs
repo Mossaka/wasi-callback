968b0828761f26a3fc81b0199b6bbf436573a0b1
Jiaxiao Zhou <jiazho@microsoft.com>
2022-06-29 18:00:06 -0700

    no need to for customized events with 2 stores
    Signed-off-by: Jiaxiao Zhou <jiazho@microsoft.com>

--- src/main.rs
+++ src/main.rs
@@ -6,13 +6,14 @@ use std::{
 
 use anyhow::Result;
 use event_handler::{EventHandler, EventHandlerData};
-use events::ExecTables;
+// use events::ExecTables;
 use wasi_cap_std_sync::WasiCtxBuilder;
 use wasi_common::{StringArrayError, WasiCtx};
 use wasmtime::{Config, Engine, Instance, Linker, Module, Store};
 use wasmtime_wasi::*;
 
 wit_bindgen_wasmtime::import!("event-handler.wit");
+wit_bindgen_wasmtime::export!("exec.wit");
 
 #[derive(Default)]
 pub struct Exec {
@@ -21,19 +22,18 @@ pub struct Exec {
     observables: Vec<MyObservable>,
 }
 
-impl events::Exec for Exec {
-    type Context = Context;
+impl exec::Exec for Exec {
     type Events = ();
 
-    fn events_get(&mut self, data: &mut Self::Context) -> Result<Self::Events, events::Error> {
+    fn events_get(&mut self) -> Result<Self::Events, exec::Error> {
         Ok(())
     }
 
     fn events_listen(
         &mut self,
         self_: &Self::Events,
-        ob: events::Observable<'_>,
-    ) -> Result<Self::Events, events::Error> {
+        ob: exec::Observable<'_>,
+    ) -> Result<Self::Events, exec::Error> {
         let _ob = MyObservable {
             rd: ob.rd.to_string(),
             key: ob.key.to_string(),
@@ -42,17 +42,11 @@ impl events::Exec for Exec {
         Ok(())
     }
 
-    fn events_exec(
-        &mut self,
-        data: &mut Self::Context,
-        self_: &Self::Events,
-        duration: u64,
-    ) -> Result<(), events::Error> {
+    fn events_exec(&mut self, self_: &Self::Events, duration: u64) -> Result<(), exec::Error> {
         let mut thread_handles = vec![];
         for i in 0..10 {
-            let mut host = data.host.as_ref().unwrap().lock().unwrap();
-            let handler = host.guest_handler.as_ref().unwrap().clone();
-            let store = host.guest_store.as_mut().unwrap().clone();
+            let handler = self.guest_handler.as_ref().unwrap().clone();
+            let store = self.guest_store.as_mut().unwrap().clone();
             thread_handles.push(thread::spawn(move || {
                 let mut store = store.lock().unwrap();
                 let _res = handler
@@ -72,28 +66,23 @@ impl events::Exec for Exec {
 pub struct GuestExec {
     data: EventHandlerData,
 }
-impl events::Exec for GuestExec {
-    type Context = GuestContext;
+
+impl exec::Exec for GuestExec {
     type Events = ();
 
-    fn events_get(&mut self, data: &mut Self::Context) -> Result<Self::Events, events::Error> {
+    fn events_get(&mut self) -> Result<Self::Events, exec::Error> {
         Ok(())
     }
 
     fn events_listen(
         &mut self,
         self_: &Self::Events,
-        ob: events::Observable<'_>,
-    ) -> Result<Self::Events, events::Error> {
+        ob: exec::Observable<'_>,
+    ) -> Result<Self::Events, exec::Error> {
         Ok(())
     }
 
-    fn events_exec(
-        &mut self,
-        data: &mut Self::Context,
-        self_: &Self::Events,
-        duration: u64,
-    ) -> Result<(), events::Error> {
+    fn events_exec(&mut self, self_: &Self::Events, duration: u64) -> Result<(), exec::Error> {
         Ok(())
     }
 }
@@ -102,40 +91,17 @@ fn main() -> Result<()> {
     let engine = Engine::new(&default_config()?)?;
     let path = "target/wasm32-wasi/release/demo.wasm";
 
-    let ctx = Context {
-        wasi: default_wasi()?,
-        host: None,
-        host_tables: None,
-    };
-    let guest_ctx = GuestContext {
-        wasi: default_wasi()?,
-        host: GuestExec {
-            data: EventHandlerData::default(),
-        },
-    };
+    let (mut store, mut linker, instance) = wasmtime_init(&engine, path)?;
+    let (mut store2, mut linker2, instance2) = wasmtime_init(&engine, path)?;
 
-    let (mut store, mut linker, module) = wasmtime_init(&engine, ctx, path)?;
-    let (mut store2, mut linker2, module2) = wasmtime_init(&engine, guest_ctx, path)?;
-
-    events::add_to_linker(&mut linker, |cx: &Context| {
-        (
-            cx.host.as_ref().unwrap().clone(),
-            cx.host_tables.as_ref().unwrap().clone(),
-        )
+    let handler = EventHandler::new(&mut store2, &instance2, |cx: &mut GuestContext| {
+        &mut cx.host.data
     })?;
-    let instance = linker.instantiate(&mut store, &module)?;
-    store.data_mut().host_tables = Some(Arc::new(Mutex::new(ExecTables::default())));
-
-    events::add_to_linker_dummy::<GuestExec>(&mut linker2)?;
-    let instance2 = linker2.instantiate(&mut store2, &module2)?;
-
-    let handler = EventHandler::new(&mut store2, &instance2, |cx: &mut _| &mut cx.host.data)?;
-    store.data_mut().host = Some(Arc::new(Mutex::new(Exec {
+    store.data_mut().host = Exec {
         guest_handler: Some(Arc::new(Mutex::new(handler))),
         guest_store: Some(Arc::new(Mutex::new(store2))),
         observables: vec![],
-    })));
-
+    };
     instance
         .get_typed_func::<(), (), _>(&mut store, "_start")?
         .call(&mut store, ())?;
@@ -163,49 +129,37 @@ pub fn default_wasi() -> Result<WasiCtx, StringArrayError> {
     Ok(ctx.build())
 }
 
-pub fn wasmtime_init<T: Ctx>(
+pub fn wasmtime_init<T: exec::Exec + Default>(
     engine: &Engine,
-    ctx: T,
     path: &str,
-) -> Result<(Store<T>, Linker<T>, Module)>
+) -> Result<(Store<Context<T>>, Linker<Context<T>>, Instance)>
 where
 {
+    let ctx = Context::default();
     let mut linker = Linker::new(&engine);
     let mut store = Store::new(&engine, ctx);
     let module = Module::from_file(&engine, path)?;
-    wasmtime_wasi::add_to_linker(&mut linker, |cx: &mut T| cx.wasi_mut())?;
-
-    Ok((store, linker, module))
+    wasmtime_wasi::add_to_linker(&mut linker, |cx: &mut Context<T>| &mut cx.wasi)?;
+    exec::add_to_linker(&mut linker, |cx: &mut Context<T>| {
+        (&mut cx.host, &mut cx.host_tables)
+    })?;
+    let instance = linker.instantiate(&mut store, &module)?;
+    Ok((store, linker, instance))
 }
 
-pub struct Context {
+pub struct Context<T>
+where
+    T: exec::Exec + Default,
+{
     pub wasi: WasiCtx,
-    pub host: Option<Arc<Mutex<Exec>>>,
-    pub host_tables: Option<Arc<Mutex<ExecTables<Exec>>>>,
+    pub host: T,
+    pub host_tables: exec::ExecTables<T>,
 }
 
-pub struct GuestContext {
-    pub wasi: WasiCtx,
-    pub host: GuestExec,
-}
-
-pub trait Ctx {
-    fn wasi_mut(&mut self) -> &mut WasiCtx;
-}
-
-impl Ctx for Context {
-    fn wasi_mut(&mut self) -> &mut WasiCtx {
-        &mut self.wasi
-    }
-}
-
-impl Ctx for GuestContext {
-    fn wasi_mut(&mut self) -> &mut WasiCtx {
-        &mut self.wasi
-    }
-}
-
-impl Default for Context {
+impl<T> Default for Context<T>
+where
+    T: exec::Exec + Default,
+{
     fn default() -> Self {
         Self {
             wasi: default_wasi().unwrap(),
@@ -215,16 +169,9 @@ impl Default for Context {
     }
 }
 
-impl Default for GuestContext {
-    fn default() -> Self {
-        Self {
-            wasi: default_wasi().unwrap(),
-            host: Default::default(),
-        }
-    }
-}
-
 pub struct MyObservable {
     pub rd: String,
     pub key: String,
 }
+
+pub type GuestContext = Context<GuestExec>;
